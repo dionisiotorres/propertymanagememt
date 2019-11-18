3754bef935ab7bdfa19d231cf4feaa56861b31e6
Aung Myo <aungmyoswe@ZANDO.local>
2019-11-18 10:08:39 +0630

    "Update Request"

--- property_management_system/models/pms_api_integration.py
+++ property_management_system/models/pms_api_integration.py
@@ -1,6 +1,6 @@
 import requests
 from odoo import api, fields, models
-from requests_oauth2 import OAuth2BearerToken
+# from requests_oauth2 import OAuth2BearerToken
 from odoo.addons.property_management_system.models import api_rauth_config
 
 

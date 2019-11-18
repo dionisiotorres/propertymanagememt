6360153077af46de6956bbfe74d9e54168009453
Aung Myo <aungmyoswe@ZANDO.local>
2019-11-18 14:51:26 +0630

    "Update rauth2 module"

--- property_management_system/requests_oauth2/__init__.py
+++ property_management_system/requests_oauth2/__init__.py
@@ -1,3 +1,3 @@
-from requests_oauth2.oauth2 import OAuth2
-from requests_oauth2.bearer import OAuth2BearerToken
-from requests_oauth2.errors import OAuth2Error, ConfigurationError
+from . import OAuth2
+from . import OAuth2BearerToken
+from . import OAuth2Error, ConfigurationError

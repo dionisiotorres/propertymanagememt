4283b53c4ce9eb723ec5514b43f6f2087f5cec98
Aung Myo <aungmyoswe@ZANDO.local>
2019-12-20 09:57:20 +0630

    "upate pos daily sale"

--- property_management_system/__manifest__.py
+++ property_management_system/__manifest__.py
@@ -13,7 +13,7 @@
     'category':
     'base',
     'version':
-    '1.0.66',
+    '1.0.67',
     'depends': ['base', 'contacts', 'uom', 'account', 'mail'],
     'data': [
         'security/property_security.xml',

b7c5cd600f4cb47409a07b015afcf19aef098a05
Aung Myo <aungmyoswe@ZANDO.local>
2020-01-16 15:18:42 +0630

    "Space Type Update"

--- property_management_system/models/pms_applicable_space_type.py
+++ property_management_system/models/pms_applicable_space_type.py
@@ -4,11 +4,17 @@ from odoo import models, fields, api
 class PMSApplicableSpaceType(models.Model):
     _name = 'pms.applicable.space.type'
     _description = 'Applicable Space Type'
+    _order = 'ordinal_no,name'
 
-    name = fields.Char("Name", required=True, track_visibility=True)
-    space_type = fields.Many2one("pms.space.type", "Type", required=True)
-    chargeable = fields.Boolean("Chargeable", track_visibility=True)
-    divisible = fields.Boolean("Divisible", track_visibility=True)
+    name = fields.Char("Space Type", required=True, track_visibility=True)
+    space_type_id = fields.Many2one("pms.space.type",
+                                    "Main Space Type",
+                                    required=True)
+    chargeable = fields.Boolean("IsChargable", track_visibility=True)
+    divisible = fields.Boolean("Can be divided", track_visibility=True)
+    ordinal_no = fields.Integer("Ordinal No",
+                                required=True,
+                                help='To display order as prefer')
     active = fields.Boolean(default=True)
 
     @api.multi

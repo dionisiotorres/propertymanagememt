8fb1ca6393f4c90976a67925c886d1e9ceb14d78
Aung Myo <aungmyoswe@ZANDO.local>
2020-03-25 14:16:16 +0630

    "UPDATED DATA"

--- property_management_system/__manifest__.py
+++ property_management_system/__manifest__.py
@@ -13,7 +13,7 @@
     'category':
     'base',
     'version':
-    '1.0.4',
+    '1.0.5',
     'depends': ['base', 'contacts', 'uom', 'account', 'mail', 'web'],
     'data': [
         'security/property_security.xml',
